f599a6ff75b0b1f4801a5b16486564a5946a77b9
mrkuz <markus@bitsandbobs.net>
2020-03-01 21:23:20 +0100

    Set up org-protocol

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,16 @@
 if status --is-interactive
-  alias emacs "emacsclient -c -a emacs"
+  alias emacs "env SHELL=/bin/bash emacsclient -c -a emacs"
   abbr --add --global gco "git checkout"
   abbr --add --global gs "git status"
   abbr --add --global gd "git diff"
   abbr --add --global gdc "git diff --cached"
   abbr --add --global gpo "git push -u origin"
+end
+
+function todo
+  if test (count $argv) -eq 0
+    echo "Usage: todo TITLE"
+  else
+    emacsclient -n -c -F '((name . "org-protocol-capture"))' "org-protocol://capture://b/$argv"
+  end
 end
