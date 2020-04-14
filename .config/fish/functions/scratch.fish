96205667d7cb55ff9c1065d1ff1839d0f339ceaf
mrkuz <markus@bitsandbobs.net>
2020-04-14 15:01:14 +0200

    Update scratch to use new docker image

--- .config/fish/functions/scratch.fish
+++ .config/fish/functions/scratch.fish
@@ -1,8 +1,8 @@
 function scratch
   if test -n "$argv[1]"
     set name "$argv[1]"
-    docker start -i $name || x11d --hostname $name -ti --name $name mrkuz/ubuntu-base
+    docker start -i $name || x11d --hostname $name -ti --name $name mrkuz/ubuntu-base-x11d
   else
-    x11d --hostname scratch -ti --rm mrkuz/ubuntu-base
+    x11d --hostname scratch -ti --rm mrkuz/ubuntu-base-x11d
   end
 end
