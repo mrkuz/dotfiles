43a51e176576263959b52b234264155b98be875d
mrkuz <markus@bitsandbobs.net>
2020-06-21 19:40:01 +0200

    Improve x11d

--- .config/fish/functions/scratch.fish
+++ .config/fish/functions/scratch.fish
@@ -1,8 +1,8 @@
 function scratch
   if test -n "$argv[1]"
     set name "$argv[1]"
-    docker start -i $name || x11d --hostname $name --name $name mrkuz/ubuntu-desktop
+    docker start -i $name || x11d -k -n $name mrkuz/ubuntu-desktop
   else
-    x11d --hostname scratch --rm mrkuz/ubuntu-desktop
+    x11d -n scratch mrkuz/ubuntu-desktop
   end
 end
--- .config/fish/functions/x11d.fish
+++ .config/fish/functions/x11d.fish
@@ -1,8 +1,32 @@
 function x11d
+  argparse -x s,m 'h/help' 's/share-home' 'm/mount-home' 'k/keep-container' 'n/name=' -- $argv
+  if set -q _flag_h
+    echo -n "Usage: x11d OPTIONS IMAGE
+
+Options:
+  -h, --help
+  -k, --keep-container
+  -m, --mount-home
+  -n, --name
+  -s, --share-home
+"
+    return
+  end
+  if set -q _flag_s
+    set argv -v /home/(id -un):/home/(id -un)/shared/ $argv
+  else if set -q _flag_m
+    set argv -v /home/(id -un):/home/(id -un)/ $argv
+  end
+  if not set -q _flag_k
+    set argv --rm $argv
+  end
+  if set -q _flag_n
+    set argv --name $_flag_n --hostname $_flag_n $argv
+  end
+
   docker run \
     --user (id -u):(id -g) \
     -ti \
-    -v /data/docker-home/(id -un):/home/(id -un) \
     --security-opt apparmor=unconfined \
     --ipc host \
     --gpus all \
