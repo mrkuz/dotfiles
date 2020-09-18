b70c6153da103e9639c96cd5f50146e84a7b00d0
mrkuz <markus@bitsandbobs.net>
2020-09-18 14:45:15 +0200

    Remove unused fish functions

--- .config/fish/functions/scratch.fish
+++
@@ -1,8 +0,0 @@
-function scratch
-  if test -n "$argv[1]"
-    set name "$argv[1]"
-    docker start -i $name || x11d -k -n $name mrkuz/ubuntu-desktop
-  else
-    x11d -n scratch mrkuz/ubuntu-desktop
-  end
-end
