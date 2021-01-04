82716239850ef53e1e2c3646e54cb4814e1c1906
mrkuz <markus@bitsandbobs.net>
2021-01-04 16:10:15 +0100

    Remove vscode aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,9 +7,6 @@ if status --is-interactive
 
   alias docker-purge "docker rm (docker ps -aq)"
   alias kube-clean "kubectl get pods --all-namespaces | grep Evicted | awk '{print \" -n \" \$1 \" \" \$2}' | xargs kubectl delete pod"
-  alias dcode "code --user-data-dir ~/.vscode/DevOps --extensions-dir ~/.vscode/DevOps/extensions"
-  alias jcode "code --user-data-dir ~/.vscode/Java --extensions-dir ~/.vscode/Java/extensions"
-  alias jscode "code --user-data-dir ~/.vscode/JavaScript --extensions-dir ~/.vscode/JavaScript/extensions"
   alias ubuntu-shell="docker run -ti -u (id -un) -h ubuntu-shell -v /data/overlay/home/mnt/(id -un):/home/(id -un) -v /nix:/nix:ro -v /run/current-system:/run/current-system:ro --rm mrkuz/ubuntu-shell"
   alias copy="xclip -selection clipboard"
   alias paste="xclip -o -selection clipboard"
