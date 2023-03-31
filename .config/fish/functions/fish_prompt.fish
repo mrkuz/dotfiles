b36b9dbdcd45771f52bf1acfc9cc14bb4928f68a
mrkuz <markus@bitsandbobs.net>
2023-04-01 00:47:29 +0200

    Fix fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -42,7 +42,7 @@ function fish_prompt
     end
   end
 
- if test -f /.dockerenv or test -f /run/.containerenv
+ if test -f /.dockerenv -o -f /run/.containerenv
     echo -n " ["
     set_color brred
     echo -n "container"
